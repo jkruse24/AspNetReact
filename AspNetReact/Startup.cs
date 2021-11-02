c30cb1a22b87c64b7597edb6afb794d2abde10a5
AURORA_NT\kruse <jake.kruse@fenetech.com>
2021-11-02 14:54:14 -0400

    Update gitignore and added Microsoft.AspNetCore.SpaServices.Extensions

--- AspNetReact/Startup.cs
+++ AspNetReact/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
